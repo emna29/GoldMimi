09936832453b11af68ea97d2b66ae7f1f17a4250
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-03-30 19:42:01 +0100

    Added link to the products in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_180910) do
+ActiveRecord::Schema.define(version: 2019_03_30_184013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_03_30_180910) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "picture"
+    t.string "link"
   end
 
   create_table "rings", force: :cascade do |t|
